b3429ea7f322f09a8ef3c02a5d9eba7c79a12ac7
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-13 17:25:08 -0500

    logs7

--- csye6225-fall2018-update-lambda.sh
+++ csye6225-fall2018-update-lambda.sh
@@ -1,11 +1,36 @@
-#!/usr/bin/env bash
-pwd
-ls -al
+#!/bin/bash
 
-set -e lambdaName=$(aws lambda list-functions --region us-east-1 --query 'Functions[0].FunctionName' --output text)
 
-echo "Name of lambda function is "$lambdaName
 
-echo "updating {$lambdaName} function"
+SRC_LAMBDA_FUNCTION_NAME="$1"
+DST_LAMBDA_FUNCTION_NAME="$1"
 
-aws lambda update-function-code --zip-file=file://lambda_deployment/lambda-dpk-nmn.zip --region=us-east-1 --function-name=$lambdaName
+SRC_PACKAGE_URL=$(aws lambda get-function --function-name "$SRC_LAMBDA_FUNCTION_NAME" --query 'Code.Location' --output text)
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+TMP_FILE_PATH=$(mktemp)
+
+echo "Downloading source package of $SRC_LAMBDA_FUNCTION_NAME..."
+
+curl --silent "$SRC_PACKAGE_URL" -o "$TMP_FILE_PATH"
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Updating code of $DST_LAMBDA_FUNCTION_NAME..."
+
+aws lambda update-function-code --function-name "$DST_LAMBDA_FUNCTION_NAME" --zip-file "fileb://$TMP_FILE_PATH" --publish >/dev/null
+
+UPDATE_RESULT=$?
+
+if [ -f "$TMP_FILE_PATH" ]; then
+    rm -f "$TMP_FILE_PATH"
+fi
+
+if [ $UPDATE_RESULT -ne 0 ]; then
+    exit 1
+fi
