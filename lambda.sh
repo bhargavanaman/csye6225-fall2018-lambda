94844bc30182cffbc852886a35e1fc1f176443fd
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-13 18:22:54 -0500

    logs18

--- lambda.sh
+++ lambda.sh
@@ -4,8 +4,8 @@ pwd
 ls -al
 set -e MyLambda=$(aws lambda list-functions --region us-east-1 --query 'Functions[0].FunctionName' --output text)
 
-echo "$MyLambda”
+echo $MyLambda
 
 echo "updating function”
 
-aws lambda update-function-code --zip-file=fileb://lambda-aws-ROOT.zip --region=us-east-1 --function-name="$MyLambda”
+aws lambda update-function-code --zip-file=fileb://lambda-aws-ROOT.zip --region=us-east-1 --function-name=$MyLambda
