7c6a32e240224f5e136e2f66b6e465ee714a281a
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-13 17:13:57 -0500

    logs6

--- csye6225-fall2018-update-lambda.sh
+++ csye6225-fall2018-update-lambda.sh
@@ -4,8 +4,8 @@ ls -al
 
 set -e lambdaName=$(aws lambda list-functions --region us-east-1 --query 'Functions[0].FunctionName' --output text)
 
-echo "Name of lambda function is $lambdaName”
+echo "Name of lambda function is "$lambdaName
 
-echo "updating $lambdaName function”
+echo "updating {$lambdaName} function"
 
 aws lambda update-function-code --zip-file=file://lambda_deployment/lambda-dpk-nmn.zip --region=us-east-1 --function-name=$lambdaName
