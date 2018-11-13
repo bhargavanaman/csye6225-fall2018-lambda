055d63c2c127437556854af1929d193cf0d98e81
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-13 18:52:42 -0500

    logs25

--- lambda.sh
+++ lambda.sh
@@ -3,4 +3,5 @@ echo "Inside lambda.sh file"
 pwd
 ls -al
 MyLambda=$(aws lambda list-functions --region us-east-1 --query 'Functions[0].FunctionName' --output text)
+aws lambda update-function-configuration --function-name $MyLambda --memory-size 150 --timeout 600
 aws lambda update-function-code --zip-file=fileb://lambda_artifact/lambda-dpk-nmn.zip --region=us-east-1 --function-name=$MyLambda
