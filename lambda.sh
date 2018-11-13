a897bc5f4f16f5438696efee7b1a41e131fa0cc4
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-13 18:39:11 -0500

    logs22

--- lambda.sh
+++ lambda.sh
@@ -3,4 +3,4 @@ echo "Inside lambda.sh file"
 pwd
 ls -al
 MyLambda=$(aws lambda list-functions --region us-east-1 --query 'Functions[0].FunctionName' --output text)
-aws lambda update-function-code --zip-file=fileb://lambda-aws-ROOT.zip --region=us-east-1 --function-name=$MyLambda
+aws lambda update-function-code --zip-file=fileb://lambda-dpk-nmn.zip --region=us-east-1 --function-name=$MyLambda
